8912273ae804a64ab0730eebcab9720bd85dc0ff
fire13542 <fire13542@gmail.com>
2020-09-19 14:35:47 +0300

    API_URL

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  // public static API_URL = 'https://o-class-virtual-classroom-api.herokuapp.com/';
-  public static API_URL = "http://localhost:3000/";
+  public static API_URL = 'https://o-class-virtual-classroom-api.herokuapp.com/';
+  // public static API_URL = "http://localhost:3000/";
 
   constructor() { }
 }
